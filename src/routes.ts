8a75db83dd0d33ce4c7a2e95c011d6ddd2418e0e
Yago Milano <yagomilano92@gmail.com>
2020-07-24 10:50:15 -0300

    criando middleware de autenticacao

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 
+import authMiddleware from './app/middlewares/authMiddleware'
+
 import UserController from './app/controllers/UserController'
 import AuthController from './app/controllers/AuthController'
 
 const router = Router()
 
 router.post('/users', UserController.store)
+router.get('/users', authMiddleware, UserController.index)
+
 router.post('/auth', AuthController.authenticate)
 
 export default router
